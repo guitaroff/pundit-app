b6142150aedf47d76f284144e94a3b15382a536d
andrey <ag82pishi@yandex.ru>
2015-09-25 23:35:01 +0300

    adds devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,20 +32,8 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
   config.include FactoryGirl::Syntax::Methods
-  
+  #config.include Devise::TestHelpers, type: :controller
+  #config.include Features::SessionHelpers, type: :feature
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 end
